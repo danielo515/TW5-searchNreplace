91d4cb0ac73c04e9152db1359c277b28000d8d0c
danielo515 <rdanielo@gmail.com>
2015-04-09 22:17:39 +0200

    Updated scripts

--- bld.cmd
+++ bld.cmd
@@ -2,11 +2,7 @@
 
 rem Build SamplePlugin for TiddlyWiki5
 
-tiddlywiki ^
-	.\SamplePluginWiki ^
+tiddlywiki.cmd ^
+	.\searchNreplacewiki ^
 	--verbose ^
 	--build ^
-	|| exit 1
-
-copy SamplePluginWiki\output\readme.md readme.md
-copy SamplePluginWiki\output\license.md license.md
--- bld.sh
+++ bld.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Build SamplePlugin for TiddlyWiki5
-
-tiddlywiki \
-	./*wiki \
-	--verbose \
-	--build \
-	|| exit 1
-
-# mv *wiki/output/readme.md readme.md
+mv *wiki/output/license.md license.md
 mv *wiki/output/readme.md readme.md
 mv *wiki/output/index.html index.html
 git add index.html
 git add readme.md
+git add license.md
 d=$(date "+%Y-%m-%d %H:%M")
 git commit -m "Build latest version $d"
